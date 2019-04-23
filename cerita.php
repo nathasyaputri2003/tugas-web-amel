abe78a38b612bd54b0e10ea83939b38f70ef5643
nathasyaputri2003 <49853550+nathasyaputri2003@users.noreply.github.com>
2019-04-23 11:48:53 +0700

    Add files via upload

---
+++ cerita.php
@@ -0,0 +1,22 @@
+    <h1 class="content teks-tengah ">Cerita Hidupku</h1>
+    <p>
+      Nama saya Nathasya Putri Amelia biasa di panggil Amel. Saya lahir di Medan pada tanggal 10 Februari 2003. Saya beragama Islam serta jenis kelamin saya Wanita. Sekarang saya bersekolah di SMK Telkom Medan.
+    </p>
+    <p>
+      Saya anak ke-2 dari 2 Bersaudara.Kakak pertama saya masih di sebuah universitas di Telkom Purwokerto.
+    </p>
+    <p>
+      Saya mulai masuk di dunia pendidikan di mulai dari TK usia 4 tahun. Saya di Sekolahkan di TK Siti Hajar. Waktu TK Saya begitu suka pada pelajaran Menggambar, saya sempat juga bercita cita menjadi ceo karena ketertarikan menjadi pemimpin di suatu perusahaan.
+    </p>
+    <p>
+     Sesudah tamat dari TK Saya masuk di SD Siti Hajar. Saya sering juga mengikuti perlombaan dan pernah mendapatkan piala lomba mewarnai kaligrafi dan fashion show. Dan saya pindah sd dari situ dan pindah ke SD Muhammadiyah 03 Medan. 
+    </p>
+    <p>
+     Sesudah lulus SD  saya meneruskan sekolah di SMP Muhammadiyah 03. Di SMP ini saya lebih suka pada mata pelajaran computer seperti TIK dan Bahasa Inggris. Sesudah lulus SMP diteruskan lagi di SMK Telkom Medan yang saat ini saya masih duduk di sekolah ini. Saya sekarang masih duduk di bangku kelas 12 dengan jurusan Rekayasa Perangkat Lunak. Saya masuk ke sekolah di bidang IT karena ketertarikan saya di bidang computer sejak dari SMP. Dengan melihat skill saya seperti ada di bidang computer akhirnya saya memutuskan untuk masuk ke sekolah di bidang computer yaitu SMK Telkom Medan.
+    </p>
+    <p>
+      Setelah tamat dari SMK saya berencana melanjutkan kuliah di Universitas Telkom Purwokerto sama dengan abang saya dengan mengambil jurusan yangs sesuai dengan jurusan sekolah saya saat ini. Saya juga berkeinginan saat kuliah nanti sudah memiliki usaha sendiri baik di bidang startup.
+    </p>
+    <p>
+    	Mungkin beginilah perjalanan hidup saya sampai sekarang. Mungkin ini yang bisa saya sampaikan. Sekian dan Terima Kasih banyak untuk pembaca	
+    </p>
---
+++ profilku.php
@@ -0,0 +1,66 @@
+
+	<h1 class="content teks-tengah">Profilku</h1>
+	<div class="row">
+	<center>
+		<table class="table table-bordered table-striped">
+			<tbody>
+			<tr>
+				<td><b>Nama Lengkap</b></td>
+				<td><b>:</b></td>
+				<td>NATHASYA PUTRI AMELIA</td>
+			</tr>
+			<tr>
+				<td><b>Nama Panggilan</b></td>
+				<td></b>	:</b> </td>
+				<td>AMEL</td>
+			</tr>
+			<tr>
+				<td><b>Tempat,Tanggal Lahir</b></td>
+				<td> :</b></td>
+				<td> Medan, 10 Februari 2003</td>
+			</tr>
+			<tr>
+				<td><b>Umur</b></td>
+				<td> :</b> </td>
+				<td>16 Tahun</td>
+			</tr>
+			<tr>
+				<td><b>Jenis Kelamin</b>
+				<td><b></b>:</b></td>
+				<td> Perempuan</td>
+			</tr>
+			<tr>
+				<td><b>Gol. Darah</b>
+				<td><b>:</b></td>
+				<td>B</td>
+			</tr>
+			<tr>
+				<td><b>Agama</b></td>
+				<td><b>:</b></td>
+				<td>Islam</td>
+			</tr>
+			<tr>
+				<td><b>Alamat</b></td>
+				<td><b>:</b></td>
+				<td>Jln. HM.Puna Sembiring</td>
+			</tr>
+			<tr>
+				<td><b>Status</b></td>
+				<td><b>:</b></td> 
+				<td>Pelajar</td>
+			</tr>
+			<tr>
+				<td><b>Pekerjaan</b></td>
+				<td><b> :</b></td>
+				<td> Pelajar</td>
+			</tr>
+			<tr>
+				<td><b>Kewarganegaraan</b></td>
+				<td><b>:</b></td>
+				<td> Indonesia</td>
+			</tr>
+			</tbody>
+		</table>
+		</center>
+	</div>
+
